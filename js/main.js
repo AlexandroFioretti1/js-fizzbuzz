e3a2e5b64b349ddb38697279c11f515df49b99ea
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-03-11 14:38:21 +0100

    add number 1\100 and fix fizz\buzz+fizzbuzz

--- js/main.js
+++ js/main.js
@@ -16,24 +16,26 @@ const divElement = document.querySelector(".container");
 console.log(divElement);
 
 // Scrivi un programma che stampi in console i numeri da 1 a 100(ciclo for)
-for (let i = 1; i < 101; i++) {
-  const squareEl =
-    (divElement.innerHTML += `<div class="square d-flex align-items-center justify-content-center m-3"> ${i} </div>`);
+for (let i = 1; i <= 100; i++) {
+  let x = i;
 
-  // per i numeri multipli di 3 che di 5 stampi “FizzBuzz”.
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
+    x = "fizzBuzz";
+    console.log(i, x);
 
     // per i multipli di 3 stampi “Fizz” al posto del numero
-    squareEl.innerHTML = "FizzBuzz";
   } else if (i % 3 === 0) {
-    console.log("Fizz");
+    x = "Fizz";
+
+    console.log(i, x);
 
     // per i multipli di 5 stampi “Buzz”
   } else if (i % 5 === 0) {
-    console.log("Buzz");
-    // per mostrare i numeri da 1 a 100
+    x = "Buzz";
+
+    console.log(i, x);
   } else {
     console.log(i);
   }
+  divElement.innerHTML += `<div class="square d-flex align-items-center justify-content-center m-3"> ${x} </div>`;
 }
